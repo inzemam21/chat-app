3cef0afc62e7069102d31d00929e47226f6112db
Inzimam Ul Haq <inzemamsmile2@gmail.com>
2025-03-07 20:44:32 +0400

    added websocket connected

--- main.go
+++ main.go
@@ -4,10 +4,44 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+
+	"github.com/gorilla/websocket"
 )
+
+var upgrader = websocket.Upgrader{
+	ReadBufferSize:  1024,
+	WriteBufferSize: 1024,
+	CheckOrigin:     func(r *http.Request) bool { return true }, // Allow all origins for simplicity
+}
+
+func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+	conn, err := upgrader.Upgrade(w, r, nil)
+	if err != nil {
+		log.Println("Upgrade error:", err)
+		return
+	}
+	defer conn.Close()
+
+	fmt.Println("Client connected")
+
+	for {
+		_, message, err := conn.ReadMessage()
+		if err != nil {
+			fmt.Println("Client disconnected")
+			return
+		}
+		fmt.Printf("Received: %s\n", message)
+		err = conn.WriteMessage(websocket.TextMessage, message) // Echo back the message
+		if err != nil {
+			fmt.Println("Write error:", err)
+			return
+		}
+	}
+}
 
 func main() {
 	http.Handle("/", http.FileServer(http.Dir("./static")))
+	http.HandleFunc("/ws", handleWebSocket) // New WebSocket endpoint
 
 	fmt.Println("Server starting on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
