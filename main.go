a2a2f5cb4685a63cffa77ff7f48ef465c7f99273
Inzimam Ul Haq <inzemamsmile2@gmail.com>
2025-03-07 20:45:58 +0400

    managing multi clients with hub

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"sync"
 
 	"github.com/gorilla/websocket"
 )
@@ -11,37 +12,108 @@ import (
 var upgrader = websocket.Upgrader{
 	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
-	CheckOrigin:     func(r *http.Request) bool { return true }, // Allow all origins for simplicity
+	CheckOrigin:     func(r *http.Request) bool { return true },
 }
 
-func handleWebSocket(w http.ResponseWriter, r *http.Request) {
+// Client represents a connected user
+type Client struct {
+	conn *websocket.Conn
+	send chan []byte // Channel to send messages to this client
+}
+
+// Hub manages all connected clients
+type Hub struct {
+	clients    map[*Client]bool // Map of connected clients
+	broadcast  chan []byte      // Channel for broadcasting messages
+	register   chan *Client     // Channel for new client connections
+	unregister chan *Client     // Channel for client disconnections
+	mutex      sync.Mutex       // For thread-safe map access
+}
+
+func NewHub() *Hub {
+	return &Hub{
+		clients:    make(map[*Client]bool),
+		broadcast:  make(chan []byte),
+		register:   make(chan *Client),
+		unregister: make(chan *Client),
+	}
+}
+
+func (h *Hub) Run() {
+	for {
+		select {
+		case client := <-h.register:
+			h.mutex.Lock()
+			h.clients[client] = true
+			h.mutex.Unlock()
+			fmt.Printf("Client connected. Total: %d\n", len(h.clients))
+
+		case client := <-h.unregister:
+			h.mutex.Lock()
+			if _, ok := h.clients[client]; ok {
+				close(client.send)
+				delete(h.clients, client)
+			}
+			h.mutex.Unlock()
+			fmt.Printf("Client disconnected. Total: %d\n", len(h.clients))
+
+		case message := <-h.broadcast:
+			h.mutex.Lock()
+			for client := range h.clients {
+				select {
+				case client.send <- message:
+				default: // If client isn't receiving, remove it
+					close(client.send)
+					delete(h.clients, client)
+				}
+			}
+			h.mutex.Unlock()
+		}
+	}
+}
+
+func (h *Hub) handleWebSocket(w http.ResponseWriter, r *http.Request) {
 	conn, err := upgrader.Upgrade(w, r, nil)
 	if err != nil {
 		log.Println("Upgrade error:", err)
 		return
 	}
-	defer conn.Close()
 
-	fmt.Println("Client connected")
+	client := &Client{conn: conn, send: make(chan []byte, 256)}
+	h.register <- client
 
+	defer func() {
+		h.unregister <- client
+		conn.Close()
+	}()
+
+	// Handle sending messages to the client
+	go func() {
+		defer conn.Close()
+		for message := range client.send {
+			err := conn.WriteMessage(websocket.TextMessage, message)
+			if err != nil {
+				return
+			}
+		}
+	}()
+
+	// Handle receiving messages from the client
 	for {
 		_, message, err := conn.ReadMessage()
 		if err != nil {
-			fmt.Println("Client disconnected")
 			return
 		}
-		fmt.Printf("Received: %s\n", message)
-		err = conn.WriteMessage(websocket.TextMessage, message) // Echo back the message
-		if err != nil {
-			fmt.Println("Write error:", err)
-			return
-		}
+		h.broadcast <- message
 	}
 }
 
 func main() {
+	hub := NewHub()
+	go hub.Run()
+
 	http.Handle("/", http.FileServer(http.Dir("./static")))
-	http.HandleFunc("/ws", handleWebSocket) // New WebSocket endpoint
+	http.HandleFunc("/ws", hub.handleWebSocket)
 
 	fmt.Println("Server starting on :8080...")
 	log.Fatal(http.ListenAndServe(":8080", nil))
